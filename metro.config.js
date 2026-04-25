895cb838d830ff06ed631e284b4c1e81a2b59c40
LucKorn <lucianokorn@outlook.com>
2026-04-25 08:05:40 -0300

    Update metro.config.js to modify asset extensions

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
 module.exports = config;
